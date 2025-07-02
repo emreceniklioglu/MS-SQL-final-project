89aaf38018355cbda295a0c7dea9dd40bf60c5c7
Emre Ceniklioğlu <154685699+emreceniklioglu@users.noreply.github.com>
2025-07-02 17:49:41 +0300

    Delete 5.Veri Temizleme ve ETL Süreçleri Tasarımı directory

--- 5.Veri Temizleme ve ETL Süreçleri Tasarımı/veri yükleme/veriekleme.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM [AdventureWorks2022].[Purchasing].[ShipMethod]
-
-INSERT INTO [Purchasing].[ShipMethod] ([Name],[ShipBase],[ShipRate],[ModifiedDate])
-	VALUES 
-		('CARGO-X',10.55,1.34, '2025-04-22 00:00:00.000')
-
-SELECT * FROM [AdventureWorks2022].[Purchasing].[ShipMethod]
