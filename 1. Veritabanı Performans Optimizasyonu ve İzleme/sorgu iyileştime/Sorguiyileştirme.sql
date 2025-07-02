3fa86122b126ee5538fe9adc242baeb8e5f45cab
Emre Ceniklioğlu <154685699+emreceniklioglu@users.noreply.github.com>
2025-07-02 17:48:35 +0300

    Delete 1. Veritabanı Performans Optimizasyonu ve İzleme directory

--- 1. Veritabanı Performans Optimizasyonu ve İzleme/sorgu iyileştime/Sorguiyileştirme.sql
+++
@@ -1,9 +0,0 @@
--- sorgu
-SELECT * FROM [AdventureWorks2022].[Production].[WorkOrder]
-WHERE ProductID = 3;
-
--- sorguyu optimizie etmek için nonclustered index tanýmlýyoruz
-CREATE NONCLUSTERED INDEX index_Productid 
-ON [AdventureWorks2022].[Production].[WorkOrder] (ProductID); 
-
-SELECT * FROM [AdventureWorks2022].[Production].[WorkOrder] WHERE ProductID= 3;
--- 1. Veritabanı Performans Optimizasyonu ve İzleme/sorgu iyileştime/Sorguiyileştirme2.sql
+++
@@ -1,9 +0,0 @@
--- sorgu
-SELECT * FROM [AdventureWorks2022].[Sales].[SalesOrderDetail]
-WHERE SalesOrderDetailID = 102482;
-
--- sorguyu optimizie etmek için nonclustered index tanýmlýyoruz
-CREATE NONCLUSTERED INDEX index_Productid 
-ON [AdventureWorks2022].[Sales].[SalesOrderDetail] (SalesOrderDetailID); 
-
-SELECT * FROM [Sales].[SalesOrderDetail] WHERE SalesOrderDetailID = 102482;
