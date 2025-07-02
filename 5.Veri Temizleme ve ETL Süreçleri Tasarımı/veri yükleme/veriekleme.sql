255b9bc28b96ebf2577ef0126b115c075d12bc92
Emre Ceniklioğlu <154685699+emreceniklioglu@users.noreply.github.com>
2025-07-02 18:34:10 +0300

    Add files via upload

---
+++ 5.Veri Temizleme ve ETL Süreçleri Tasarımı/veri yükleme/veriekleme.sql
@@ -0,0 +1,7 @@
+SELECT * FROM [AdventureWorks2022].[Purchasing].[ShipMethod]
+
+INSERT INTO [Purchasing].[ShipMethod] ([Name],[ShipBase],[ShipRate],[ModifiedDate])
+	VALUES 
+		('CARGO-X',10.55,1.34, '2025-04-22 00:00:00.000')
+
+SELECT * FROM [AdventureWorks2022].[Purchasing].[ShipMethod]
